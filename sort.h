85a3ebe25d068d8509d2ffee6c6191bf615ab3db
taha <tahahitman1@gmail.com>
2024-12-27 20:00:45 +0330

    created the logic for sorting the array

--- sort.h
+++ sort.h
@@ -1,3 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int temp = 0;
 void SortAndPrint(int* array,int size){
-    
+    for(int i = 0 ; i < size ; i++){
+        for(int j =i+1; j < size; j++){
+            if(array[j] < array[i]){
+                temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+    for(int i = 0 ; i < size ; i ++){
+        cout << array[i] << endl;
+    }
 }
